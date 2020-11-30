3bbbed5ac424e075251717b246de958b507d5707
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-11-30 18:38:21 -0300

    adding initial files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+
+export default function App() {
+
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" component={SignIn} />
+            </Switch>
+        </Router>
+    );
+}
